d33ee6bd99075c78f4f0b7141515ce9924d48a94
callazar <callazar1234@gmail.com>
2023-01-05 10:36:33 +0700

    update, 5 jan

---
+++ src/components/homepage/items.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from "vue";
+import itemcategories from "../itemscategories.vue"
+
+const items = ref([
+    { id: 1, title: "Mobile UI kit", text: "Mobile UI Kit", image: "items-1.jpg" },
+    { id: 2, title: "Online Doctor Consultation", text: "Website UI Kit", image: "items-2.jpg" },
+    { id: 3, title: "Banking Crypto", text: "Mobile UI Kit", image: "items-3.jpg" }
+])
+
+</script>
+
+<template>
+    <div class="container px-4 mx-auto my-16 md:px-12">
+        <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">New Items</h2>
+        <div class="flex flex-wrap -mx-1 lg:-mx-4">
+
+            <itemcategories v-for="item in items" :key="item.id" :title="item.title" :text="item.text"
+                :image="item.image" />
+        </div>
+    </div>
+</template>
--- src/components/layouts/navbar.vue
+++ src/components/layouts/navbar.vue
@@ -7,11 +7,13 @@ import userinfo from "./userinfo.vue"
 <template>
     <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
         <div class="container flex flex-wrap items-center justify-between mx-auto my-2">
+
             <logo />
 
             <userinfo />
 
             <navlink />
+            
         </div>
     </nav>
 </template>
