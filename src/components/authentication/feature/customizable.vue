fd30fe42f15cf028ece90267d89462e532f41fc9
callazar <callazar1234@gmail.com>
2023-01-05 14:42:50 +0700

    update, 5 jan 14.42

---
+++ src/components/authentication/feature/customizable.vue
@@ -0,0 +1,18 @@
+<script setup>
+    import iconlayer from "@/components/icon/iconlayer.vue"
+</script>
+
+<template>
+    <div class="mb-12">
+        <div class="block mb-2">
+            <iconlayer />
+            <h3 class="inline-block ml-2 text-lg font-semibold">
+                Customizable
+            </h3>
+        </div>
+        <p class="mt-2 text-lg text-gray-500">
+            Accelerate your work with our millions <br />
+            of ready-to-use design projects.
+        </p>
+    </div>
+</template>
---
+++ src/components/authentication/featurelist.vue
@@ -0,0 +1,11 @@
+
+<script setup>
+import customizable from "@/components/authentication/feature/customizable.vue"
+import fortune from "@/components/authentication/feature/fortune.vue"
+import documentation from "@/components/authentication/feature/documentation.vue"
+</script>
+<template>
+    <customizable />
+    <fortune />
+    <documentation />
+</template>
