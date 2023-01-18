6759bac505a9d574d989681d551182a04a8c9170
callazar <callazar1234@gmail.com>
2023-01-18 09:39:39 +0700

    update 18 jan

---
+++ src/components/detail/Gallery.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+import { ref } from "vue";
+
+const DefaultImage = ref("gallery-1.png");
+const galleries = ref([
+  { id: 1, Image: "gallery-2.png" },
+  { id: 2, Image: "gallery-3.png" },
+  { id: 3, Image: "gallery-4.png" },
+  { id: 4, Image: "gallery-5.png" },
+]);
+</script>
+
+<template>
+  <section id="gallery">
+    <img
+      :src="'/src/assets/img/' + DefaultImage"
+      alt=""
+      class="w-full mt-6 rounded-2xl"
+    />
+    <div class="grid grid-cols-4 gap-4 mt-4">
+      <div class="overflow-hidden cursor-pointer rounded-2xl">
+        <img src="@/assets/img/gallery-2.png" class="w-full" alt="" />
+      </div>
+      <div class="overflow-hidden cursor-pointer ring-2 ring-indigo-500 rounded-2xl">
+        <img src="@/assets/img/gallery-3.png" class="w-full" alt="" />
+      </div>
+      <div class="overflow-hidden cursor-pointer rounded-2xl">
+        <img src="@/assets/img/gallery-4.png" class="w-full" alt="" />
+      </div>
+      <div class="overflow-hidden cursor-pointer rounded-2xl">
+        <img src="@/assets/img/gallery-5.png" class="w-full" alt="" />
+      </div>
+    </div>
+  </section>
+</template>
